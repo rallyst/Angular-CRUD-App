ebecc6f564f83285b53b6e33375ecb9fa4375716
Roman <rallyshops@gmail.com>
2022-04-01 16:24:21 +0300

    added note card

--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  AfterViewInit,
+  Component, 
+  ElementRef, 
+  Input, 
+  OnInit, 
+  Renderer2, 
+  ViewChild 
+} from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.scss']
 })
-export class NoteCardComponent implements OnInit {
+export class NoteCardComponent implements OnInit, AfterViewInit{
 
-  constructor() { }
+  @Input() title!: string;
+  @Input() body!: string;
 
-  ngOnInit(): void {
+  @ViewChild('truncator', { static: true}) truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true}) bodyText!: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {   
+  }
+
+  ngAfterViewInit(): void {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    } 
   }
 
 }
+function Import() {
+  throw new Error('Function not implemented.');
+}
+
